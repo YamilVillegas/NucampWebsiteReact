fb3261c20907089d2e6aebaa979da53c4b3e4c51
Yamil Villegas <YTorres87@gmail.com>
2021-11-20 12:10:07 -0500

    About Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
@@ -33,7 +34,7 @@ class Main extends Component {
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        }
+        } 
         
         const CampsiteWithId = ({match}) => {
             return (
@@ -50,6 +51,7 @@ class Main extends Component {
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                        <Route exact path ='/aboutus' render={() => <About partners={this.state.partners} /> } />
                         <Route exact path='/directory/:campsiteId' component={CampsiteWithId} />
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to='/home' />
